da986702fe2718abef7e984b00c0a09f2e6ee6e0
DY Dev <dydev420@gmail.com>
2026-04-20 04:56:09 +0530

    feat: add read file tool in request

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             ],
             "model": model,
+            "tools": [
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "Read",
+                        "description": "Read and return the contents of a file",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "file_path": {
+                                    "type": "string",
+                                    "description": "The path to the file to read"
+                                }
+                            },
+                            "required": ["file_path"]
+                        }
+                    }
+                }
+            ],
         }))
         .await?;
 
