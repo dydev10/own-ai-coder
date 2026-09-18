7418771f8c33d6d07320f948c3cf085724f524bc
DY Dev <dydev420@gmail.com>
2026-09-18 20:39:56 +0530

    chore: update your_program.sh to work with new name on codecrafter run

--- your_program.sh
+++ your_program.sh
@@ -14,11 +14,11 @@ set -e # Exit early if any commands fail
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cargo build --release --target-dir=/tmp/codecrafters-build-claude-code-rust --manifest-path Cargo.toml
+  cargo build --release --target-dir=/tmp/owncode-rust --manifest-path Cargo.toml
 )
 
 # Copied from .codecrafters/run.sh
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec /tmp/codecrafters-build-claude-code-rust/release/codecrafters-claude-code "$@"
+exec /tmp/owncode-rust/release/owncode "$@"
